879bbba84f3341694d57067ba7ded1de7977c736
avijit7 <avijit1257.it@gmail.com>
2017-10-07 18:39:59 +0530

    ready for heroku deploy

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -1,6 +1,9 @@
 module Api
   module V1
     class BooksController < BaseController
+      before_filter :restrict_access
+      respond_to :json
+      	
       def index
 	    @results = Book.search(params[:q])
 	    
